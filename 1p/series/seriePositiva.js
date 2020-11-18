83f1ca9913ffc93e761b8b9069e2a37adc7055fb
danieldevcode <danieldevcode@gmail.com>
2020-11-17 19:32:52 -0600

    prueba

---
+++ 1p/series/seriePositiva.js
@@ -0,0 +1,19 @@
+/*
+Autor: Cortés Comparán Daniel
+Fecha: 26 de octubre del 2020
+Estado: Completo
+*/
+
+function serie(){
+    let x=1,
+        y=2,
+        n=10,
+        k=1;
+    
+    for(k; k<=n; k++){
+        console.log(x);
+        x+=y;
+    }
+}
+
+serie();
